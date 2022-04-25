5291922d0122cabdabb28d85f4764617a83874a3
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2022-04-25 14:34:00 +0200

    security

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 mongoose.connect('mongodb+srv://biboundee:biboundee@cluster0.a5y1q.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true,
