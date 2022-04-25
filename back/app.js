615ef20dc5a574cb6f64861cfd681d87e3cf43d4
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2022-04-25 15:09:52 +0200

    files

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
 });
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 mongoose.connect('mongodb+srv://biboundee:biboundee@cluster0.a5y1q.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true,
